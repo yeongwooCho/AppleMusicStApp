34ae095c34a9d9414b1d5a701793ac0caade251a
yeongwoo <jyy0223@naver.com>
2021-02-14 18:02:58 +0900

    home group start

---
+++ AppleMusicStApp/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  AppleMusicStApp
+//
+//  Created by 조영우 on 2021/02/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
